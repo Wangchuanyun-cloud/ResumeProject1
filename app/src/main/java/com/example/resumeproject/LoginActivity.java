6463a6e6916f84f17d06654bf631e0faf7e6342b
wenjing <1150716051@qq.com>
2018-08-04 12:01:13 +0800

    123

--- app/src/main/java/com/example/resumeproject/LoginActivity.java
+++ app/src/main/java/com/example/resumeproject/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.resumeproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        TextView textView;
     }
 }
