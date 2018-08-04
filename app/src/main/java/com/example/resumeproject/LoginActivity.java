a02ce9eaf198f9818a4d38563809bf52f6423078
wenjing <1150716051@qq.com>
2018-08-04 12:22:35 +0800

    123

--- app/src/main/java/com/example/resumeproject/LoginActivity.java
+++ app/src/main/java/com/example/resumeproject/LoginActivity.java
@@ -1,8 +1,8 @@
 package com.example.resumeproject;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.TextView;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
